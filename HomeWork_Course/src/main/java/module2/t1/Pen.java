aa769d9d0e243ec838854ed925c27898d21078bf
Ksenomorfa <black_estrella@mail.ru>
2017-05-08 16:50:23 +0300

    * Added task 1: Pen and PenTest
    Signed-off-by: Ksenomorfa <black_estrella@mail.ru>

---
+++ HomeWork_Course/src/main/java/module2/t1/Pen.java
@@ -0,0 +1,44 @@
+package module2.t1;
+
+/*
+TASK: Разработайте спецификацию и создайте класс Ручка (Pen).
+Определите в этом классе методы equals(), hashCode() и toString().
+ */
+public class Pen {
+    String manufactor;
+    String model;
+    double price;
+
+    @Override
+    public String toString() {
+        return "Pen manufactor: " + manufactor + ", model: " + model + ", price: " + price;
+    }
+    @Override
+    public int hashCode(){
+        return (int)(price*21)+ ((null == manufactor) ? 0 : manufactor.hashCode())
+                              + ((null == model)? 0 :model.hashCode());
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if(this==obj) return true;
+        if(null==obj) return false;
+        if(getClass()!=obj.getClass()) return false;
+        Pen pen = (Pen)obj;
+        if ((manufactor==pen.manufactor)
+                &&((model==pen.model))
+                &&(price==pen.price)) return true;
+        return false;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setManufactor(String manufactor) {
+        this.manufactor = manufactor;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
