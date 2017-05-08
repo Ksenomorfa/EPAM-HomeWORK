b89e453dff5ed6a5026c0c15e6f64efcbb3ea027
Ksenomorfa <black_estrella@mail.ru>
2017-05-08 20:40:57 +0300

    * Corrected > in docs
    Signed-off-by: Ksenomorfa <black_estrella@mail.ru>

--- HomeWork_Course/src/main/java/module1/t1/main/Main.java
+++ HomeWork_Course/src/main/java/module1/t1/main/Main.java
@@ -4,10 +4,10 @@ import module1.t1.logic.Logic;
 
 /**
  * <p>Compiling from command line with command:</p>
- * <code>C:\Users\Полина\IdeaProjects\EPAM-HomeWORK\HomeWork_Course>
+ * <code>C:\Users\Полина\IdeaProjects\EPAM-HomeWORK\HomeWork_Course
  * javac -d bin -cp src\main\java\ src\main\java\module1\t1\main\Main.java</code>
  * <p>Running from command line with command:</p>
- * <code>C:\Users\Полина\IdeaProjects\EPAM-HomeWORK\HomeWork_Course>
+ * <code>C:\Users\Полина\IdeaProjects\EPAM-HomeWORK\HomeWork_Course
  * java -cp ./bin module1.t1.main.Main</code>
  * <p>Result:</p> I am string in Logic
  */
