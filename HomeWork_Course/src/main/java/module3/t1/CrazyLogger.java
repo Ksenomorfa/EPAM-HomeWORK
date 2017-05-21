cc54c3debffc4ca0e82d23cac7ce84bb3d5f533b
Ksenomorfa <black_estrella@mail.ru>
2017-05-21 13:22:28 +0300

    * Add description of task to doc in Task1/Module3
    Signed-off-by: Ksenomorfa <black_estrella@mail.ru>

--- HomeWork_Course/src/main/java/module3/t1/CrazyLogger.java
+++ HomeWork_Course/src/main/java/module3/t1/CrazyLogger.java
@@ -10,6 +10,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
+ * Описание задачи:
+ * Задание 1. Строковые классы
+ * Необходимо создать класс CrazyLogger, ведущий журнал лога, используя как
+ * накопитель объект типа StringBuilder. Логгер должен содержать методы поиска
+ * определенной информации в таком логе [с возможностью вывода ее в поток ввода вывода].
+ * Информацию логгер хранит в форматированном виде: dd-mm-YYYY : hh-mm – message;.
+
  * Class with crazy logging: can log whatever you want to log and can not log what you do not want to log.
  * <p>
  * Adding to class can be realized with
