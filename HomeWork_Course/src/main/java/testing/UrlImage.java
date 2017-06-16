2527b405a60db4e19de382a46dff0efaa1bbcd49
Ksenomorfa <black_estrella@mail.ru>
2017-06-16 22:27:05 +0300

    * Fixed Streams and regexp
    Signed-off-by: Ksenomorfa <black_estrella@mail.ru>

--- HomeWork_Course/src/main/java/testing/UrlImage.java
+++ HomeWork_Course/src/main/java/testing/UrlImage.java
@@ -1,34 +1,32 @@
 package testing;
 
+import org.apache.commons.io.IOUtils;
+
 import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UrlImage {
-    public String urlConnectionReader() throws IOException {
-        URL url = new URL("http://horstmann.com");
+    public String readImageFilesFromURL(String urlString) throws IOException {
+        URL url = new URL("http://" + urlString);
         URLConnection urlConnection = url.openConnection();
         StringBuilder string = new StringBuilder();
-        final int bufferSize = 4096;
-        final char[] buffer = new char[bufferSize];
-        try (InputStreamReader reader = new InputStreamReader(urlConnection.getInputStream())) {
-            while (reader.ready()) {
-                int rsz = reader.read(buffer, 0, buffer.length);
-                string.append(buffer, 0, rsz);
-            }
+        try (InputStream reader = urlConnection.getInputStream()) {
+            string.append(IOUtils.toString(reader, StandardCharsets.UTF_8));
         }
-        List list = getImageAddresses(string.toString(), url);
+        List<String> list = getImageAddresses(string.toString(), url);
         getImagesFromURL(list, url);
         return string.toString();
     }
 
-    public List<String> getImageAddresses(String inputString, URL url) {
+    private List<String> getImageAddresses(String inputString, URL url) {
         List<String> imagesList = new ArrayList<>();
-        Pattern pattern = Pattern.compile("<img.*(src=\")(.*\\.(gif|jpg))");
+        Pattern pattern = Pattern.compile("<img.*(src=\")(.*?)(?=\\\")");
         Matcher matcher = pattern.matcher(inputString);
         while (matcher.find()) {
             imagesList.add(matcher.group(2));
@@ -36,21 +34,16 @@ public class UrlImage {
         return imagesList;
     }
 
-    public void getImagesFromURL(List<String> list, URL url) throws IOException {
+    private void getImagesFromURL(List<String> list, URL url) throws IOException {
         for (String string : list) {
             URL urlPicture = new URL("http://" + url.getHost() + "/" + string);
             URLConnection urlConnection = urlPicture.openConnection();
-            try (InputStream reader = urlConnection.getInputStream()) {
-                try (ByteArrayOutputStream result2 = new ByteArrayOutputStream()) {
-                    File pictureName = new File(string);
-                    OutputStream outputStream = new FileOutputStream(pictureName);
-                    byte[] buffer = new byte[1024];
-                    int length;
-                    while ((length = reader.read(buffer))!=-1) {
-                        result2.write(buffer,0,length);
-                    }
-                    result2.writeTo(outputStream);
-                }
+            File pictureName = new File(string);
+            try (InputStream reader = urlConnection.getInputStream();
+                 OutputStream outputStream = new FileOutputStream(pictureName)) {
+                byte[] buffer = new byte[reader.available()];
+                reader.read(buffer);
+                outputStream.write(buffer);
             }
         }
     }
--- HomeWork_Course/src/test/java/testing/UrlImageTest.java
+++ HomeWork_Course/src/test/java/testing/UrlImageTest.java
@@ -4,15 +4,10 @@ import org.junit.Test;
 
 import java.io.IOException;
 
-import static org.junit.Assert.*;
-
 public class UrlImageTest {
     @Test
     public void testReading() throws IOException {
         UrlImage urlImage = new UrlImage();
-        System.out.println(urlImage.urlConnectionReader());
-
+        System.out.println(urlImage.readImageFilesFromURL("horstmann.com"));
     }
-
-
 }
