a1d60e0fe272692e0d6da39fc4cdec492b261ae2
Ksenomorfa <black_estrella@mail.ru>
2017-07-17 16:13:56 +0300

    * MyHashMap with removing from LinkedList in bucket
    Signed-off-by: Ksenomorfa <black_estrella@mail.ru>

--- HomeWork_Course/src/test/java/testing/Structures/MyHashMapTest.java
+++ HomeWork_Course/src/test/java/testing/Structures/MyHashMapTest.java
@@ -5,10 +5,18 @@ import org.junit.Test;
 public class MyHashMapTest {
     @Test
     public void testCreateHashMap() {
-        MyHashMap<String, String> map = new MyHashMap<>();
-        map.put("key1", "value1");
-        map.put("key2", "value2");
-        map.put("key3", "value3");
-        map.delete("key3");
+        MyHashMap<String, String> map = new MyHashMap<>(6);
+        map.put("3","21");
+        map.put("4","22");
+        map.put("13","23");
+        map.put("22","24");
+        map.put("31","25");
+        System.out.println(map);
+        map.remove("4");
+        map.put("35","22");
+        map.remove("22");
+        map.remove("3");
+        map.put("4","22");
+        System.out.println(map);
     }
 }
