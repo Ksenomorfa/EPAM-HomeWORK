faf7b88ef0c482fd8d93f178577ccf0dd419d3e0
Ksenomorfa <black_estrella@mail.ru>
2017-08-27 21:53:18 +0300

    * Semaphore philosophers with classes
    Signed-off-by: Ksenomorfa <black_estrella@mail.ru>

--- HomeWork_Course/src/main/java/multithreading/SemaphorePhilosophersWithClasses.java
+++ HomeWork_Course/src/main/java/multithreading/SemaphorePhilosophersWithClasses.java
@@ -1,44 +1,43 @@
 package multithreading;
 
-import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 
-public class SyncPhilosophersWithClasses {
+public class SemaphorePhilosophersWithClasses {
     public static final int NUMBER_OF_PHILOSOPHERS = 5;
 
     public static void main(String[] args) throws InterruptedException {
         ExecutorService service = Executors.newFixedThreadPool(NUMBER_OF_PHILOSOPHERS);
+        Semaphore[] semaphores = new Semaphore[NUMBER_OF_PHILOSOPHERS];
+
         int firstFork = 0;
         int lastFork = NUMBER_OF_PHILOSOPHERS - 1;
-        Fork[] mutex = new Fork[NUMBER_OF_PHILOSOPHERS];
-
-        for (int i = 0; i < NUMBER_OF_PHILOSOPHERS; i++) {
-            mutex[i] = new Fork(i);
+        for (int i = 0; i < semaphores.length; i++) {
+            semaphores[i] = new Semaphore(1);
         }
-        System.out.println(Arrays.asList(mutex));
 
         for (int i = 0; i < NUMBER_OF_PHILOSOPHERS; i++) {
             if (i < lastFork) {
-                service.submit(new Philosopher(i, i, i + 1, mutex));
+                service.submit(new PhilosopherSem(i, i, i + 1, semaphores));
             } else {
-                service.submit(new Philosopher(lastFork, lastFork, firstFork, mutex));
+                service.submit(new PhilosopherSem(lastFork, lastFork, firstFork, semaphores));
             }
         }
     }
 }
 
-class Philosopher implements Runnable {
+class PhilosopherSem implements Runnable {
     private final int pos;
     private final int leftFork;
     private final int rightFork;
-    private final Fork[] mutex;
+    private final Semaphore[] sems;
 
-    Philosopher(int pos, int leftFork, int rightFork, Fork[] mutex) {
+    PhilosopherSem(int pos, int leftFork, int rightFork, Semaphore[] sems) {
         this.pos = pos;
         this.leftFork = leftFork;
         this.rightFork = rightFork;
-        this.mutex = mutex;
+        this.sems = sems;
     }
 
     @Override
@@ -49,25 +48,26 @@ class Philosopher implements Runnable {
     }
 
     private void eat() {
-        synchronized (mutex[leftFork]) {
-            System.out.println("Phylosoph number: " + pos + " put up left fork " + leftFork);
-            synchronized (mutex[rightFork]) {
-                System.out.println("Phylosoph number: " + pos + " put up right fork " + rightFork);
-                System.out.println("Phylosoph number: " + pos + " eating ");
-                Sleep.sleepMillis(2000);
-            }
-            System.out.println("Phylosoph number: " + pos + " put off right fork " + rightFork);
-        }
+        sems[leftFork].acquireUninterruptibly(1);
+        sems[rightFork].acquireUninterruptibly(1);
+        System.out.println("Phylosoph number: " + pos + " put up left fork " + leftFork);
+        System.out.println("Phylosoph number: " + pos + " put up right fork " + rightFork);
+        System.out.println("Phylosoph number: " + pos + " eating ");
+        Sleep.sleepMillis(2000);
+
+        sems[rightFork].release();
+        sems[leftFork].release();
+        System.out.println("Phylosoph number: " + pos + " put off right fork " + rightFork);
         System.out.println("Phylosoph number: " + pos + " put off left fork " + leftFork);
         System.out.println("Phylosoph number: " + pos + " thinking");
         Sleep.sleepMillis(1000);
     }
 }
 
-class Fork {
+class ForkSem {
     private int pos;
 
-    Fork(int pos) {
+    ForkSem(int pos) {
         this.pos = pos;
     }
 
